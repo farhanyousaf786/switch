38ca2b060d3c42bb1ed78f8c8fb270c86253cf35
Farhan Yousaf <farhanyousaf1996@gmail.com>
2022-07-31 17:20:46 -0400

    Update UI

---
+++ lib/Models/Constans.dart
@@ -0,0 +1,67 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+class Constants {
+  static int notifyCounter = 0; // to control Notification when app opens
+  static String uploadUrl = "";
+  static String myName = "";
+  static String mySecondName = "";
+  static String myEmail = "";
+  static String myId = "";
+  static String myPhotoUrl = "";
+  static String globalGroupChatId = "";
+  static String mood = "";
+  static String moodUrl = "";
+  static String gender = "";
+  static String country = "";
+  static String dob = "";
+  static String about = "";
+  static String username = "";
+  static String isVerified = "";
+  static String groupChatId = "";
+  static String follower = "";
+  static String following = "";
+  static List allMemes = [];
+  static String notificationContent = "";
+  static String notificationType = "";
+  static String switchId = "l9YFPY9yDqNqFcsi5";
+  static String switchIdFarhan = "T32SFNtIWMTsozx7Hefeweg2";
+  static String switchIdLaaSY = "h84QW4Tl9CdRXomduiNFfeGUv1iY2";
+  static String pass = "";
+  static String isIntro = "";
+  static String isIntroForMemeProfile = "";
+  static String introForChatListPage = "";
+
+  static String isDark = "false";
+
+
+
+  //
+
+
+  static bool notifyBell = true;
+  static bool messageIconActive = false;
+  static String isMessageReply = "no";
+
+
+
+
+
+
+  String inRelationshipWithId = "";
+  static String token = "";
+
+  static String currentMoodGlobal = "happy";
+  static int counter = 10;
+
+  static const double padding = 20;
+  static const double avatarRadius = 45;
+
+
+  ///*** Love Note Variables ***\\\
+
+  static String content = "";
+  static String imageUrl = "";
+  static String timeStamp = "";
+}
